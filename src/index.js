0fabc2dedbf2d5ab02907ff456a9d34885711692
Nicholas Clawson <nickclaw@gmail.com>
2016-01-29 00:30:40 -0800

    Update with latest module versions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import { Ability, intent } from 'alexa-ability';
+/** @jsx ssml */
+
+import { Ability, events } from 'alexa-ability';
 import handle from 'alexa-ability-lambda-handler';
+import { ssml } from 'alexa-ssml';
 
 const ability = new Ability();
 
-ability.on(intent.LAUNCH, function(sess) {
-    sess.say("Testing testing one two three.").send();
+ability.on(events.launch, function(req) {
+    const response = (
+        <speak>
+            Testing testing
+            <pause time={300} />
+            one two three.
+        </speak>
+    );
+
+    req.say(response).show("Shuttle launched!");
 });
 
-ability.on('ProblemIntent', function(sess) {
-    sess.say("Okay, stand by, 13. We're looking at it.").end();
+ability.on('ProblemIntent', function(req) {
+    req.say("Okay, stand by, 13. We're looking at it.").end();
+});
+
+ability.on(events.error, function(req) {
+    req.say("We have a problem over here too.");
 });
 
 export const handler = handle(ability);
